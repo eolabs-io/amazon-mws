22f76e8c47f57f453f57c3269b90534864783016
tim hall <timhall@tims-mbp.lan>
2020-06-28 11:13:33 -0500

    clean up

--- src/Domain/Orders/Jobs/PerformFetchListOrderItems.php
+++ src/Domain/Orders/Jobs/PerformFetchListOrderItems.php
@@ -16,20 +16,6 @@ class PerformFetchListOrderItems implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * The number of times the job may be attempted.
-     *
-     * @var int
-     */
-    // public $tries = 0;
-
-    /**
-     * The number of seconds the job can run before timing out.
-     *
-     * @var int
-     */
-    // public $timeout = 30;
-
     /** @var EolabsIo\AmazonMws\Domain\Orders\ListOrderItems */
     public $listOrderItems;
 
@@ -60,15 +46,5 @@ class PerformFetchListOrderItems implements ShouldQueue
     {
         return [AmazonMwsThrottlingMiddleware::forListOrderItems()];
     }
-
-    /**
-     * Calculate the number of seconds to wait before retrying the job.
-     *
-     * @return int
-     */
-    // public function retryAfter()
-    // {
-    //     return 1 * pow(2, $this->attempts());
-    // }
 
 }
--- src/Domain/Orders/Jobs/PerformFetchListOrders.php
+++ src/Domain/Orders/Jobs/PerformFetchListOrders.php
@@ -16,19 +16,6 @@ class PerformFetchListOrders implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     
-    /**
-     * The number of times the job may be attempted.
-     *
-     * @var int
-     */
-    // public $tries = 0;
-
-    /**
-     * The number of seconds the job can run before timing out.
-     *
-     * @var int
-     */
-    // public $timeout = 300;
 
     /** @var EolabsIo\AmazonMws\Domain\Orders\ListOrders */
     public $listOrders;
@@ -60,15 +47,5 @@ class PerformFetchListOrders implements ShouldQueue
     {
         return [AmazonMwsThrottlingMiddleware::forListOrders()];
     }
-
-    /**
-     * Calculate the number of seconds to wait before retrying the job.
-     *
-     * @return int
-     */
-    // public function retryAfter()
-    // {
-    //     return 60 * pow(2, $this->attempts());
-    // }
 
 }
